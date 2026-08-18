6972b500a1683beaf44e674cb28d8cbc03646dda
Alessandra Gonzalez <alessandra.gonzalez@ucr.ac.cr>
2026-08-17 21:15:55 -0600

    add JavaScript for crochet project with image slider and modal

---
+++ Tarea2/app.js
@@ -0,0 +1,30 @@
+const wrapper = document.querySelector(".sliderWrapper");
+
+const sliderLeftButton = document.querySelector(".slider-button-left");
+const sliderRightButton = document.querySelector(".slider-button-right");
+
+const didYouKnowButton = document.querySelector("#did-you-know-button");
+const modalContainer = document.querySelector(".modal-container");
+const modalCloseButton = document.querySelector(".modal-close-button");
+
+let position = 0;
+
+sliderLeftButton.addEventListener("click", () => {
+    wrapper.style.transform=`translateX(${position==0 ? position=0 : position+=350}px)`
+    console.log(position);
+})
+
+sliderRightButton.addEventListener("click", () => {
+    wrapper.style.transform=`translateX(${position==-1050 ? position-=0 : position-=350}px)`
+    console.log(position);
+})
+
+didYouKnowButton.addEventListener("click", () => {
+    modalContainer.style.display= "block";
+    document.body.classList.add("no-scroll")
+})
+
+modalCloseButton.addEventListener("click", () => {
+    modalContainer.style.display= "none";
+    document.body.classList.remove("no-scroll")
+})
